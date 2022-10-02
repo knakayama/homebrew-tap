5948a8b6574d26d029a6d7ab8959b5819a021902
knakayama <knakayama.sh@gmail.com>
2022-10-02 15:07:54 +0900

    Brew formula update for dv version v0.0.2

--- Formula/dv.rb
+++ Formula/dv.rb
@@ -5,23 +5,23 @@
 class Dv < Formula
   desc "A command line tool to remove AWS default VPC(s)"
   homepage "https://github.com/knakayama/dv"
-  version "9.9.9"
+  version "0.0.2"
   license "MIT"
 
   depends_on "go" => :optional
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/knakayama/dv/releases/download/v9.9.9/dv_9.9.9_Darwin_x86_64.tar.gz"
-      sha256 "0aacb9ceead336200d2aede8b467356fc3128c5a02be0d28667079578413a16c"
+      url "https://github.com/knakayama/dv/releases/download/v0.0.2/dv_0.0.2_Darwin_x86_64.tar.gz"
+      sha256 "5c8a01f35a939a4f210019da699574e10075bba30982a3f87712607831304f6a"
 
       def install
         bin.install "dv"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/knakayama/dv/releases/download/v9.9.9/dv_9.9.9_Darwin_arm64.tar.gz"
-      sha256 "976604b59c404bb2da3b46b3d01b5a6c53865d79be306a37905338b1eb6002c7"
+      url "https://github.com/knakayama/dv/releases/download/v0.0.2/dv_0.0.2_Darwin_arm64.tar.gz"
+      sha256 "7d643ac7bb4000808a70281bb4cda1683af43d43d26c0a9cdd260b1af8dddd2a"
 
       def install
         bin.install "dv"
@@ -31,16 +31,16 @@ class Dv < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/knakayama/dv/releases/download/v9.9.9/dv_9.9.9_Linux_arm64.tar.gz"
-      sha256 "af32057972dcf3d67861f3a834a569465aa446d93f54936d8b5eddf4ea9425e6"
+      url "https://github.com/knakayama/dv/releases/download/v0.0.2/dv_0.0.2_Linux_arm64.tar.gz"
+      sha256 "f521ccf5c4acdd5beb19a07c4259bdede83d70b7a2758c80f5dde0d3fd74bd04"
 
       def install
         bin.install "dv"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/knakayama/dv/releases/download/v9.9.9/dv_9.9.9_Linux_x86_64.tar.gz"
-      sha256 "403710f8b75be3d2c70c561cda4cd618dfb3a92ea5f0dedce4cef1e2d7fb51e8"
+      url "https://github.com/knakayama/dv/releases/download/v0.0.2/dv_0.0.2_Linux_x86_64.tar.gz"
+      sha256 "0f9f91c2cfdfd51c693f612f7ac5d318962ac0df8ddd097b52f1fc823d9cda31"
 
       def install
         bin.install "dv"
